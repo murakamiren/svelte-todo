349fa2ba023b5443ac348c422912f60d682b8431
renmurakami <autoeyy@gmail.com>
2022-02-14 13:43:59 +0900

    make header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
-	import logo from "./assets/svelte.png";
-	import Counter from "./lib/Counter.svelte";
+	import Header from "./lib/Header.svelte";
 	import Tailwindcss from "./Tailwindcss.svelte";
 </script>
 
 <Tailwindcss />
-<main>
-	<img src={logo} alt="Svelte Logo" />
-	<h1 class="text-5xl">Hello world!</h1>
-
-	<Counter />
-</main>
+<Header />
---
+++ src/lib/Header.svelte
@@ -0,0 +1,5 @@
+<header class="w-screen">
+	<div class="w-full h-16 flex items-center bg-gray-900 shadow-md px-8">
+		<h1 class="text-white text-2xl font-bold">SVELTE TODO APP</h1>
+	</div>
+</header>
