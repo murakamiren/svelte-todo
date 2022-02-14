dc46a65343a786df8be497a7b3f46a44e331d049
renmurakami <autoeyy@gmail.com>
2022-02-14 15:19:24 +0900

    todo style

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,5 @@
 <header class="w-screen">
-	<div class="w-full h-16 flex items-center bg-gray-900 shadow-md px-8">
-		<h1 class="text-white text-2xl font-bold">SVELTE TODO APP</h1>
+	<div class="w-full h-16 flex items-center bg-gray-900 shadow-md px-24">
+		<h1 class="text-white text-xl font-semibold">Svelte Todo App</h1>
 	</div>
 </header>
--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,16 +1,78 @@
+<script lang="ts">
+	let test: string = "";
+	let todoList: string[] = [];
+	let todoInput: string;
+
+	let isDone: boolean = false;
+
+	type todoStyles = {
+		text: string;
+		p: string;
+		doneBtn: string;
+	};
+
+	const undoneStyle: todoStyles = {
+		text: "done",
+		p: "w-full",
+		doneBtn:
+			"shrink-0 py-2 px-4 mx-2 border rounded text-cyan-500 shadow hover:text-white hover:bg-cyan-500 transition ease-out",
+	};
+	const doneStyle: todoStyles = {
+		text: "not done",
+		p: "w-full text-emerald-500 line-through",
+		doneBtn:
+			"shrink-0 py-2 px-4 mx-2 border rounded text-gray-500 shadow hover:text-white hover:bg-gray-500  transition ease-out",
+	};
+
+	let isDoneStyle = undoneStyle;
+
+	const handleSubmit = () => {
+		test = todoInput;
+		todoList = [...todoList, todoInput];
+		todoInput = "";
+		console.log(todoList);
+	};
+
+	const handleIsDone = () => {
+		isDone = !isDone;
+		if (isDone) {
+			isDoneStyle = doneStyle;
+		} else {
+			isDoneStyle = undoneStyle;
+		}
+	};
+</script>
+
 <main>
 	<div class="flex flex-col items-center">
 		<h2 class="pb-1 pt-8 font-bold text-3xl w-8/12 text-cyan-500">Your Todo List</h2>
-		<div class="w-8/12 flex justify-center">
+		<div class="w-8/12 flex justify-center mb-8">
 			<input
 				type="text"
 				class="appearance-none border rounded w-full py-2 px-3 text-gray-500 shadow mr-4 outline-none focus:border-cyan-500 focus:border-2 focus:outline-none"
 				placeholder="add todo"
+				bind:value={todoInput}
 			/>
 			<button
 				class="py-2 px-4 border rounded font-semibold text-cyan-500 shadow hover:text-white hover:bg-cyan-500 transition ease-out"
-				>ADD</button
+				on:click={handleSubmit}>ADD</button
 			>
+		</div>
+		<div class="w-8/12 mb-4">
+			{#if todoList.length === 0}
+				<p class="text-emerald-500 text-2xl text-center uppercase font-semibold">there is no task!</p>
+			{:else}
+				{#each todoList as todo}
+					<div class="w-full flex mb-4">
+						<p class={isDoneStyle.p}>{todo}</p>
+						<button class={isDoneStyle.doneBtn} on:click={handleIsDone}>{isDoneStyle.text}</button>
+						<button
+							class="shrink-0 py-2 px-4 border rounded text-red-500 shadow hover:text-white hover:bg-red-500 transition ease-out"
+							>remove</button
+						>
+					</div>
+				{/each}
+			{/if}
 		</div>
 	</div>
 </main>
